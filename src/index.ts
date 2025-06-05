6d43ac0f244a1f5654d2e88933224a4d6437ec06
Julian Kim <kimjulian@grinnell.edu>
2025-06-05 10:14:45 -0500

    Split off null check into separate function

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import { JSDOM } from "jsdom";
 import { Scamper } from "scamper";
 
+function throwNullError(element: string): never {
+    throw new Error(`${element} was null`);
+}
+
 const dom = new JSDOM("<div id='output'></div>");
 global.document = dom.window.document;
 global.HTMLElement = dom.window.HTMLElement;
-const outputPane: HTMLElement = dom.window.document.getElementById("output") ?? (() => {
-    throw new Error("output pane must exist in jsdom")
-})();
+const outputPane = dom.window.document.getElementById("output")
+    ?? throwNullError("output pane");
 
-function runProgram(src: string): string {
+function runProgram(src: string) {
     const scamper = new Scamper(outputPane, src, {
         isTracing: false, isPrintingCode: false, defaultDisplay: true
     });
 
     scamper.runProgram();
 
-    return outputPane.outerHTML;
+    const outputChildren = Array.from(outputPane.children);
+    const scamperOutput = outputChildren.find(
+        (value) => value.className === "scamper-output"
+    ) ?? throwNullError("scamper output");
+
+    return scamperOutput.textContent ?? throwNullError("scamper text");
 }
 
 console.log(runProgram("(equal? 1 1)"))
